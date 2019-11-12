84a78271bd407b3a53af368b36cf18cc03c1352f
Gleb Dovzhenko <gleb.dovzhenko@synebo.io>
2019-11-12 12:17:49 +0200

    Fix "Default dispostion pre-populated" issue

--- src/Lead/lightning-components/softphone-component/aura/LeadSoftphoneComponent/LeadSoftphoneComponentHelper.js
+++ src/Lead/lightning-components/softphone-component/aura/LeadSoftphoneComponent/LeadSoftphoneComponentHelper.js
@@ -1266,8 +1266,8 @@
     },
 
     populateDefaultDisposition : function(component){
-        const disposition = component.get("v.dispositionValue");
-        const isDispositionNotSelected = this.checkIfDispositionNotSelected(disposition);
+        const dispositionDupValue = component.get("v.dispositionDupValue");
+        const isDispositionNotSelected = this.checkIfDispositionNotSelected(dispositionDupValue);
         if(isDispositionNotSelected){
             const defaultLog = this.createDefaultLog(component);
             this.saveDefaultLog(component, defaultLog);
@@ -1326,5 +1326,13 @@
             }
         });
         $A.enqueueAction(action);
+    },
+
+    resetDispositionDupValue : function(component){
+        component.set("v.dispositionDupValue", "");
+    },
+
+    resetDispositionValue : function(component){
+        component.set("v.dispositionValue","");
     }
 })
