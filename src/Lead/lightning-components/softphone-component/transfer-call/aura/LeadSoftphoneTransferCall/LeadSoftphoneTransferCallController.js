d83480022444b8dedaa1b8ace7ef97c0b9bc6f04
Gleb Dovzhenko <gleb.dovzhenko@synebo.io>
2019-11-21 20:15:57 +0200

    Move logic from old to new "Transfer call" component

--- src/Lead/lightning-components/softphone-component/transfer-call/aura/LeadSoftphoneTransferCall/LeadSoftphoneTransferCallController.js
+++ src/Lead/lightning-components/softphone-component/transfer-call/aura/LeadSoftphoneTransferCall/LeadSoftphoneTransferCallController.js
@@ -1,5 +1,64 @@
 ({
-    myAction : function(component, event, helper) {
+    init : function(component, event, helper) {
+        const action = component.get("c.getAgentDetails");
+        
+        const responseHandler = helper.produceResponseHandlePromise(action);
+        const errorHandler = helper.onErrorResponse;
+        const successHandler = (returnedAgents) => {
+            component.set("v.agentsList", returnedAgents); 
+        };
+        
+        responseHandler
+            .then(successHandler)
+            .catch(errorHandler);
+    },
+    closeTransferCallModel : function(component, event, helper){
+        const closeTransferModal = component.getEvent("closeTransferModal");
+        closeTransferModal.fire();
+    },
+    handleSelectAgnetChange : function(component, event, helper){
+        const agentNumber = component.find("agentSelect").get("v.value");
+        const isAgentNotSelected = (agentNumber === "");
+        component.set("v.callAgentDisable", isAgentNotSelected);
+        component.set("v.agentNumber", agentNumber);
+    },
+    handleSpecifyAgentNumberManually : function(component, event, helper){
+        component.set("v.openManualInputModal", true);
+    },
+    handleCloseManualInputModal : function(component, event, helper){
+        const phoneNumber = event.getParam("phoneNumber");
+        if(phoneNumber){
+            component.find("agentSelect").set("v.value", "");
+            component.set("v.agentNumber", phoneNumber);
+            component.set("v.callAgentDisable", false);
+        }
+        component.set("v.openManualInputModal", false);
+    },
+    handleCallAgent : function(component, event, helper){
+        const authKey = component.get("v.authKey");
+        const agentNumber = component.get("v.agentNumber");
+        const specifiedNumber = helper.formatAgentNumber(agentNumber);
+        const callerId = component.get("v.callerId");
+        const lead = component.get("v.lead");
+        const phoneNumber = helper.selectLeadNumber(callerId, lead);
+        const action = component.get("c.transferCallApi");
+        action.setParams({"authKey" : authKey,
+                          "specifiedNumber" : specifiedNumber,
+                          "phoneNumber" : phoneNumber});
+        
+        const responseHandler = helper.produceResponseHandlePromise(action);
+        const errorHandler = helper.onErrorResponse;
+        const successHandler = (isTransfered) => {
+                        if(isTransfered){
+                            const closeTransferModal = component.getEvent("closeTransferModal");
+                            closeTransferModal.fire();      
+                        } else {
+                            console.log('Transfer call is failed.');
+                        }
+        };
 
+        responseHandler
+            .then(successHandler)
+            .catch(errorHandler);
     }
 })
--- src/Lead/lightning-components/softphone-component/transfer-call/aura/LeadSoftphoneTransferCall/LeadSoftphoneTransferCallHelper.js
+++ src/Lead/lightning-components/softphone-component/transfer-call/aura/LeadSoftphoneTransferCall/LeadSoftphoneTransferCallHelper.js
@@ -1,5 +1,49 @@
 ({
-    helperMethod : function() {
-
+    produceResponseHandlePromise : function(action){
+        return new Promise(
+                $A.getCallback((resolve, reject) => {
+                    action.setCallback(this, result => {
+                        if (result.getState() === 'SUCCESS') {
+                            const responseBody = result.getReturnValue();
+                            return resolve(responseBody);
+                        }
+                        if (result.getState() === 'ERROR') {
+                          console.log('ERROR', result.getError());
+                          return reject(result.getError());
+                        }
+                    });
+                    $A.enqueueAction(action);
+                })
+        );
+    },
+    onErrorResponse : function(errors){
+        if(errors){
+            console.log("Error message: " + errors[0].message);
+        } else {
+            console.log("Unknown error");
+        }
+    },
+    formatAgentNumber : function(agentNumber){
+        return agentNumber.replace(/[^0-9\.]+/g, '');
+    },
+    selectLeadNumber : function(callerId, lead){
+        if(callerId == undefined){
+            return this.getPhoneFromLead(lead);
+        } else {
+            return callerId;
+        }
+    },
+    getPhoneFromLead : function(lead){
+        if(lead != undefined){
+            if(lead.Phone){
+                return lead.Phone;
+            } else if(lead.MobilePhone){
+                return lead.MobilePhone;
+            } else {
+                console.log("Transfer call exception occurs! No inbound and outbound lead specified");
+            }
+        } else {
+            console.log("Transfer call exception occurs! No inbound and outbound lead specified");
+        }
     }
 })
